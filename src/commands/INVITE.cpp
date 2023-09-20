8a2741ea54fd616dd1faff5c0377e57d03b8d852
lgeniaux <lgeniaux17@gmail.com>
2023-09-20 15:34:59 +0200

    Remove usage of fd in some context to prevent issues with disconnect / reconnect. (For some reason a metcat cleint will receive every message sent to the server

--- src/commands/INVITE.cpp
+++ src/commands/INVITE.cpp
@@ -4,28 +4,7 @@
 
 
 void CommandHandler::handleINVITE(const std::vector<std::string>& tokens, int client_fd, Server& server) {
-    if (tokens.size() < 3) {
-        std::string response = "Not enough arguments";
-        send(client_fd, response.c_str(), response.length(), 0);
-        return;
-    }
-
-    int target_fd = std::stoi(tokens[1]);
-    const std::string& channelName = tokens[2];
-    Channel* channel = server.getChannel(channelName);
-
-    if (!channel->isOperator(client_fd)) {
-        std::string response = "You must be an operator to invite users to this channel\n";
-        send(client_fd, response.c_str(), response.length(), 0);
-        return;
-    }
-
-    channel->inviteUser(target_fd);
-    // For operator
-    std::string response = "You have invited " + std::to_string(target_fd) + " to " + channelName + "\n";
-    send(client_fd, response.c_str(), response.length(), 0);
-
-    //For target
-    response = "You have been invited to " + channelName + " by " + std::to_string(client_fd) + "\n" ;
-    send(target_fd, response.c_str(), response.length(), 0);
+    (void)tokens;
+    (void)client_fd;
+    (void)server;
 }
