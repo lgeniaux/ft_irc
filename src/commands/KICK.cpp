983f834fe5ee8b95a64270538160d25ae59c961d
error7404 <justincollon@gmail.com>
2023-09-25 16:12:00 +0200

    fix KICK not checking if user was oped or not

--- src/commands/KICK.cpp
+++ src/commands/KICK.cpp
@@ -6,12 +6,25 @@
 void PART(std::string nickname, std::string channel, std::string message, Server& server);
 
 void CommandHandler::handleKICK(const std::vector<std::string>& tokens, int client_fd, Server& server) {
-    if (tokens.size() < 2) {
+    Channel *channel;
+    Client  client;
+
+    if (tokens.size() < 3) {
         RFC2812Handler::sendResponse(461, server.clients[client_fd], "KICK :Not enough parameters");
         return;
     }
-    if (!server.getClient(client_fd).isAuthenticated()) {
+    client = server.getClient(client_fd);
+    if (!client.isAuthenticated()) {
         RFC2812Handler::sendResponse(451, server.clients[client_fd], ":You have not registered");
+        return;
+    }
+    channel = server.getChannel(tokens[1]);
+    if (channel == NULL) {
+        RFC2812Handler::sendResponse(403, server.clients[client_fd], tokens[1] + " :No such channel");
+        return;
+    }
+    if (!channel->isOperator(client.getNickname())) {
+        RFC2812Handler::sendResponse(482, server.clients[client_fd], tokens[1] + " :You're not channel operator");
         return;
     }
     std::string message = ": User " + tokens[2] + " has been kicked";
