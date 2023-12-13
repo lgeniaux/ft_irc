2174aaea372f35ae4465ed1a2cc6813fc6dbf3cf
error7404 <error7404@gmail.com>
2023-12-13 12:02:22 +0100

    KICK: brodcast message    more checks

--- src/commands/KICK.cpp
+++ src/commands/KICK.cpp
@@ -27,8 +27,20 @@ void CommandHandler::handleKICK(const std::vector<std::string>& tokens, int clie
         RFC2812Handler::sendResponse(482, server.clients[client_fd], tokens[1] + " :You're not channel operator");
         return;
     }
-    std::string message = ": User " + tokens[2] + " has been kicked";
+    if (!channel->isInChannel(tokens[2])) {
+        RFC2812Handler::sendResponse(441, server.clients[client_fd], tokens[2] + " " + tokens[1] + " :They aren't on that channel");
+        return;
+    }
+    if (server.getFdFromNickname(tokens[2]) == -1) {
+        RFC2812Handler::sendResponse(401, server.clients[client_fd], tokens[2] + " :No such nick/channel");
+        return;
+    }
+    std::cout << tokens[1] << std::endl;
+    std::string message = ":" + client.getNickname() + " KICK " + tokens[1] + " " + tokens[2] + " ";
     if (tokens.size() > 3 && tokens[3] != ":")
-        message = " " + tokens[3];
-    PART(tokens[2], tokens[1], message, server);
+        message += tokens[3];
+    else
+        message += ":" + client.getNickname();
+    channel->broadcastMessageToChannel(message + "\r\n", server, "");
+	server.leaveChannel(channel->getName(), tokens[2]);
 }
