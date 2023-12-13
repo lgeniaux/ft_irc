d4c59c5ec4d83b0764abd83d1661111a0ae3b509
lgeniaux <lgeniaux17@gmail.com>
2023-12-13 16:38:49 +0100

    Add multiple channels join

--- src/commands/JOIN.cpp
+++ src/commands/JOIN.cpp
@@ -2,24 +2,29 @@
 #include "Channel.hpp"
 #include "Server.hpp"
 #include "RFC2812Handler.hpp"
+#include <sstream>
+#include <vector>
 
-
-void sendJoinSuccessInfo(Server& server, const std::string& channelName, int client_fd) {
+void sendJoinSuccessInfo(Server &server, const std::string &channelName, int client_fd)
+{
     RFC2812Handler rfcHandler;
-    Channel* channel = server.getChannel(channelName);
-    if (!channel) {
+    Channel *channel = server.getChannel(channelName);
+    if (!channel)
+    {
         return;
     }
-    
+
     // Send the topic of the channel
-    if (!channel->getTopic().empty()) {
+    if (!channel->getTopic().empty())
+    {
         rfcHandler.sendResponse(332, server.getClient(client_fd), channelName + " :" + channel->getTopic());
     }
 
     // Send the names list
     std::set<std::string> users = channel->getUsers();
     std::string namesList;
-    for (const std::string& nickname : users) {
+    for (const std::string &nickname : users)
+    {
         namesList += nickname + " ";
     }
     rfcHandler.sendResponse(353, server.getClient(client_fd), "=" + channelName + " :" + namesList);
@@ -28,7 +33,7 @@ void sendJoinSuccessInfo(Server& server, const std::string& channelName, int cli
 
 void CommandHandler::handleJOIN(const std::vector<std::string> &tokens, int client_fd, Server &server)
 {
-    RFC2812Handler rfcHandler; // Initialize here
+    RFC2812Handler rfcHandler;
 
     if (tokens.size() < 2)
     {
@@ -36,61 +41,70 @@ void CommandHandler::handleJOIN(const std::vector<std::string> &tokens, int clie
         return;
     }
 
-    const std::string &channelName = tokens[1];
-    Channel *channel = server.getChannel(channelName);
+    std::vector<std::string> channels;
+    std::vector<std::string> keys;
 
-    // Obtain the client's nickname based on their fd using the server's nickname to client map
-    std::string clientNickname = server.getClient(client_fd).getNickname();
-    if (clientNickname.empty())
+    // Split channels and keys
+    std::istringstream channelStream(tokens[1]);
+    std::string channel;
+    while (getline(channelStream, channel, ','))
     {
-        // Handle the error, e.g., by sending a message to the client
-        return;
-    }
-
-    if (channel != NULL)
-    {
-        // Debug Channel mode and invite status
-        std::cout << "Channel mode: " << channel->getMode('i') << std::endl;
-        std::cout << "Channel invite status: " << channel->isInvited(clientNickname) << std::endl;
-        // Debug Channel key and key status
-        std::cout << "Channel key: " << channel->checkKey(tokens[2]) << std::endl;
-        std::cout << "Channel key status: " << channel->getMode('k') << std::endl;
-
-        //Debug client command arguments
-        std::cout << "Client command arguments: " << tokens[1] << " " << tokens[2] << std::endl;
+        channels.push_back(channel);
     }
 
-    if (channel == NULL)
+    if (tokens.size() > 2)
     {
-        // Channel does not exist, create it
-        server.joinChannel(channelName, clientNickname);
-        sendJoinSuccessInfo(server, channelName, client_fd);
+        std::istringstream keyStream(tokens[2]);
+        std::string key;
+        while (getline(keyStream, key, ','))
+        {
+            keys.push_back(key);
+        }
     }
-    else if (channel->getMode('i') && !channel->isInvited(clientNickname))
-    {
-        rfcHandler.sendResponse(473, server.getClient(client_fd), channelName + " :Cannot join channel (+i)");
-    }
-    else if (channel->getMode('k') && !channel->checkKey(tokens[2]))
-    {
-        rfcHandler.sendResponse(475, server.getClient(client_fd), channelName + " :Cannot join channel (+k)");
-    }
-    else if (channel->getLimit() != 0 && channel->getUsers().size() >= channel->getLimit())
-    {
-        rfcHandler.sendResponse(471, server.getClient(client_fd), channelName + " :Cannot join channel (+l)");
-    }
-    else if (!channel->isInChannel(clientNickname))
-    {
-        server.joinChannel(channelName, clientNickname);
-        sendJoinSuccessInfo(server, channelName, client_fd);
-        std::cout << "User " << clientNickname << " joined channel " << channelName << std::endl;
 
-        channel = server.getChannel(channelName); // Refresh the channel pointer
-        if (channel != NULL && channel->isInvited(clientNickname))
-            channel->removeInvite(clientNickname);
-    }
-    else
+    for (size_t i = 0; i < channels.size(); ++i)
     {
-        // User is already in the channel
-        rfcHandler.sendResponse(443, server.getClient(client_fd), channelName + " :user already on channel");
+        const std::string &channelName = channels[i];
+        std::string key;
+        if (i < keys.size()) {
+            key = keys[i];
+        } else {
+            key = std::string();
+        }
+
+        Channel *channel = server.getChannel(channelName);
+        std::string clientNickname = server.getClient(client_fd).getNickname();
+        if (clientNickname.empty())
+            continue;
+
+        if (channel == NULL)
+        {
+            server.joinChannel(channelName, clientNickname);
+            sendJoinSuccessInfo(server, channelName, client_fd);
+        }
+        else if (channel->getMode('i') && !channel->isInvited(clientNickname))
+        {
+            rfcHandler.sendResponse(473, server.getClient(client_fd), channelName + " :Cannot join channel (+i)");
+        }
+        else if (channel->getMode('k') && !channel->checkKey(key))
+        {
+            rfcHandler.sendResponse(475, server.getClient(client_fd), channelName + " :Cannot join channel (+k)");
+        }
+        else if (channel->getLimit() != 0 && channel->getUsers().size() >= channel->getLimit())
+        {
+            rfcHandler.sendResponse(471, server.getClient(client_fd), channelName + " :Cannot join channel (+l)");
+        }
+        else if (!channel->isInChannel(clientNickname))
+        {
+            server.joinChannel(channelName, clientNickname);
+            sendJoinSuccessInfo(server, channelName, client_fd);
+            channel = server.getChannel(channelName); // Refresh the channel pointer
+            if (channel != NULL && channel->isInvited(clientNickname))
+                channel->removeInvite(clientNickname);
+        }
+        else
+        {
+            rfcHandler.sendResponse(443, server.getClient(client_fd), channelName + " :User already on channel");
+        }
     }
 }
