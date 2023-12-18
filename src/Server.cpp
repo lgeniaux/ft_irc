f055825bf0613febdb3147e8c4670409ae7852aa
error7404 <error7404@gmail.com>
2023-12-18 14:21:36 +0100

    no error message when ^C

--- src/Server.cpp
+++ src/Server.cpp
@@ -160,7 +160,6 @@ void Server::acceptClient()
 
     if (client_fd < 0)
     {
-        std::cerr << ERROR << "ERROR: Failed to fetch client fd" << std::endl;
         signalHandler(1);
         return;
     }
